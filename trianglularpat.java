49e05d047010f0a8f5e6499c2ab22693ab0495cf
Sumedh M Anvekar <sumedhm8888@gmail.com>
2023-06-23 23:56:24 +0530

    triangluar pattern

---
+++ trianglularpat.java
@@ -0,0 +1,22 @@
+import java.net.SocketPermission;
+import java.util.Scanner;
+public class trianglularpat {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+   int n,i,j,k;
+    n=sc.nextInt();
+    for(i=1;i<=n;i++)
+    {
+        for(k=n-i;k>=1;k--)
+        {
+            System.out.print(" ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    }
+}
